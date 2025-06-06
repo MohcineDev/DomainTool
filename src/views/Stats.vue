b46b958715d53db73063e43c1d5bbfd97ad2e71c
MohcineDev <mouhcinedev@gmail.com>
2025-06-06 12:48:46 +0100

    updates

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,172 +1,188 @@
 <template>
-    <table>
-        <tr>
-            <th>name</th>
-            <th>DN</th>
-            <th>d</th>
-            <th>tld taken</th>
-        </tr>
-        <tr>
-            <td class="ex">brightmarketingstrategy</td>
-            <td>463</td>
-            <td rowspan="55">31/5</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td class="ex">UnitedCleaningCompany</td>
-            <td>489</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td class="ex">KetoDietSystem</td>
-            <td>1,218</td>
-            <td>2</td>
-        </tr>
-        <tr>
-            <td class="ex">HomeCenterStores</td>
-            <td>1,198</td>
-            <td>2</td>
-        </tr>
-        <tr>
-            <td class="ex">PodcastAgency.net</td>
-            <td>635</td>
-            <td>15</td>
-        </tr>
-        <tr>
-            <td class="ex">LoadPrompt</td>
-            <td>599</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td class="ex">ifcdex</td>
-            <td>1,939</td>
-            <td>21</td>
-        </tr>
-        <tr>
-            <td class="ex">airpodscases</td>
-            <td class="r">2,565</td>
-            <td>5</td>
-        </tr>
-        <tr>
-            <td>driftvibes</td>
-            <td class="r">2,581</td>
-        </tr>
-        <tr>
-            <td class="ex">SlimPlusKeto</td>
-            <td class="r">2,565</td>
-            <td>5</td>
-        </tr>
+    <div class="table-container">
 
-        <tr>
-            <td>MobileVMS</td>
-            <td class="r">3,265</td>
-        </tr>
-        <tr>
-            <td class="ex">profacades</td>
-            <td class="r">3,258</td>
-        </tr>
-        <tr>
-            <td>ThaiBulls</td>
-            <td class="r">3,265</td>
-        </tr>
-        <tr>
-            <td>admow</td>
-            <td>1,944</td>
-        </tr>
-        <tr>
-            <td>DgBeats</td>
-            <td>1,933</td>
-        </tr>
-        <tr>
-            <td>eczemasoap</td>
-            <td>1,913</td>
-        </tr>
-        <tr>
-            <td class="ex">flsurf</td>
-            <td>1,278</td>
-            <td>2</td>
-        </tr>
-        <tr>
-            <td>dubaidemolition</td>
-            <td>555</td>
-        </tr>
-        <tr>
-            <td>btasi</td>
-            <td>1,944</td>
-        </tr>
-        <tr>
-            <td>miamihomedeveloper</td>
-            <td>524</td>
-        </tr>
-        <tr>
-            <td>IndusOverseas</td>
-            <td class="r">2,556</td>
-        </tr>
-        <tr>
-            <td>richardcknight</td>
-            <td>565</td>
-        </tr>
-        <tr>
-            <td>hausercello</td>
-            <td>2,392</td>
-        </tr>
-        <tr>
-            <td>nsblc</td>
-            <td>1,944</td>
-        </tr>
-        <tr>
-            <td>dmsksa</td>
-            <td>625</td>
-        </tr>
-        <tr>
-            <td>jscsf</td>
-            <td class="r">3,973</td>
-        </tr>
-        <tr>
-            <td>InnovationTechnologyHub</td>
-            <td class="r">3,122</td>
-        </tr>
-        <tr>
-            <td>PodcastAgency.net</td>
-            <td>635</td>
-        </tr>
-        <tr>
-            <td>vcedev</td>
-            <td>1,278</td>
-        </tr>
-        <tr>
-            <td>geteforte</td>
-            <td class="r">9,709</td>
-        </tr>
-        <tr>
-            <td>SellMyPorsche</td>
-            <td class="r">4,610</td>
-        </tr>
-        <tr>
-            <td>simbainfo</td>
-            <td>1,259</td>
-        </tr>
-        <tr>
-            <td>mediacreationtool</td>
-            <td>1,188</td>
-        </tr>
-        <tr>
-            <td>EuroMaghreb</td>
-            <td class="r">2,573</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
+        <table>
+            <tr>
+                <th>name</th>
+                <th>DN</th>
+                <th>d</th>
+                <th>tld taken</th>
+                <th>reg</th>
+            </tr>
+            <tr>
+                <td class="ex">brightmarketingstrategy</td>
+                <td>463</td>
+                <td rowspan="55">31/5</td>
+                <td>1</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">UnitedCleaningCompany</td>
+                <td>489</td>
+                <td>1</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">KetoDietSystem</td>
+                <td>1,218</td>
+                <td>2</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">HomeCenterStores</td>
+                <td>1,198</td>
+                <td>2</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">PodcastAgency.net</td>
+                <td>635</td>
+                <td>15</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">LoadPrompt</td>
+                <td>599</td>
+                <td>1</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">ifcdex</td>
+                <td>1,939</td>
+                <td>21</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td class="ex">airpodscases</td>
+                <td class="r">2,565</td>
+                <td>5</td>
+                <td>dy</td>
+            </tr>
+            <tr>
+                <td>driftvibes</td>
+                <td class="r">2,581</td>
+            </tr>
+            <tr>
+                <td class="ex">SlimPlusKeto</td>
+                <td class="r">2,565</td>
+                <td>5</td>
+            </tr>
+
+            <tr>
+                <td>MobileVMS</td>
+                <td class="r">3,265</td>
+            </tr>
+            <tr>
+                <td class="ex">profacades</td>
+                <td class="r">3,258</td>
+            </tr>
+            <tr>
+                <td>ThaiBulls</td>
+                <td class="r">3,265</td>
+            </tr>
+            <tr>
+                <td>admow</td>
+                <td>1,944</td>
+            </tr>
+            <tr>
+                <td>DgBeats</td>
+                <td>1,933</td>
+            </tr>
+            <tr>
+                <td>eczemasoap</td>
+                <td>1,913</td>
+            </tr>
+            <tr>
+                <td class="ex">flsurf</td>
+                <td>1,278</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td class="ex">dubaidemolition</td>
+                <td>555</td>
+            </tr>
+            <tr>
+                <td>btasi</td>
+                <td>1,944</td>
+            </tr>
+            <tr>
+                <td class="ex">miamihomedeveloper</td>
+                <td>524</td>
+            </tr>
+            <tr>
+                <td class="ex">IndusOverseas</td>
+                <td class="r">2,556</td>
+            </tr>
+            <tr>
+                <td>richardcknight</td>
+                <td>565</td>
+            </tr>
+            <tr>
+                <td class="ex">hausercello</td>
+                <td>2,392</td>
+            </tr>
+            <tr>
+                <td class="ex">nsblc</td>
+                <td>1,944</td>
+            </tr>
+            <tr>
+                <td>dmsksa</td>
+                <td>625</td>
+            </tr>
+            <tr>
+                <td>jscsf</td>
+                <td class="r">3,973</td>
+            </tr>
+            <tr>
+                <td class="ex">InnovationTechnologyHub</td>
+                <td class="r">3,122</td>
+            </tr>
+            <tr>
+                <td>PodcastAgency.net</td>
+                <td>635</td>
+            </tr>
+            <tr>
+                <td>vcedev</td>
+                <td>1,278</td>
+            </tr>
+            <tr>
+                <td>geteforte</td>
+                <td class="r">9,709</td>
+            </tr>
+            <tr>
+                <td>SellMyPorsche</td>
+                <td class="r">4,610</td>
+            </tr>
+            <tr>
+                <td>simbainfo</td>
+                <td>1,259</td>
+            </tr>
+            <tr>
+                <td class="ex">mediacreationtool</td>
+                <td>1,188</td>
+            </tr>
+            <tr>
+                <td>EuroMaghreb</td>
+                <td class="r">2,573</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+    <div class="ns">
+        <p>NS1.AFTERNIC.COM</p>
+        <p>ns1.atom.com</p>
+    </div>
 </template>
 
 <style scoped>
@@ -181,14 +197,42 @@ td {
     border: 1px solid #eee;
     padding: 5px;
 }
-td:not(td:first-child){
+
+th {
+    position: sticky;
+    top: 0;
+    background-color: var(--color-background);
+    border: 1px solid #eee;
+    font-size: 18px;
+}
+
+td:not(td:first-child) {
     text-align: center;
 }
+
+td {
+    font-weight: 600;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
 .ex {
     background-color: rgb(57, 20, 66);
 }
 
 .r {
     background-color: rgb(25, 72, 20);
+}
+
+.ns {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    padding: 10px;
+    border: 1px solid #aaa;
+    border-radius: 5px;
+}
+
+.ns p {
+    border-bottom: 1px solid #aaa;
 }
 </style>
